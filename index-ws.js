c574705f79ef0294947f28418cf4af7ac1295dd8
moh.fauzi <moh.fauzi@ist.id>
2025-03-24 15:06:37 +0700

    add databases

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,15 @@ server.listen(3000, function () {
   console.log("Server started on port 3000");
 });
 
+process.on("SIGINT", () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 /** Begin WebSocket */
 const WebSocketServer = require("ws").Server;
 const wss = new WebSocketServer({ server });
@@ -25,6 +34,10 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome!");
   }
 
+  db.run(
+    `INSERT INTO visitors(count, time) VALUES (${numbClients}, datetime('now'))`
+  );
+
   ws.on("close", function close() {
     console.log("Client disconnected");
   });
@@ -35,3 +48,30 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+/** End WebSocket */
+/** Begin Database */
+
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(function () {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", function (err, row) {
+    console.log(row.count);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("Database closed");
+  db.close();
+}
